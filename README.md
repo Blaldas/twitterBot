30bd92139f0e873b4b88096d8c5024faf1a14c30
Blaldas <marmingues00@gmail.com>
2020-07-30 16:39:28 +0100

    protecting the twiter codes

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 #twitterBot
 
+in order to make this program work, you will need to create an file called ''constants.txt' in the same directory where the project/node.js file is.
+In that file you will past there the bot and twitter account codes by this order:
+
+
+_______________________constants.txt________________________________________________________   
+line 1: <consumer_key>                                                                      |
+line 2: <consumer_secret>                                                                   |
+line 3: <access_token>                                                                      |
+line 4: <access_token_secret>                                                               |
+____________________________________________________________________________________________|
+
+
+ya, this was an attempt to recriate a text viewer xD
+
+
+
+
 bot que pede corpo de twitt e twitta.
 as usefull as my life :)
 
--- node.js
+++ node.js
@@ -1,43 +1,77 @@
-var Twit = require('twit');
-const prompt = require('prompt-sync')();
+    var Twit = require('twit');
+    const prompt = require('prompt-sync')();
 
-var T = new Twit({
-    consumer_key:         'wWwNyjNeBUk7vHSRs4Qbb247m',
-    consumer_secret:      'Jwrw5zbizmvYbZp30d2SbLtDoytIm3cLxHp8r9cyh7IlrdpbdH',
-    access_token:         '3979879330-3PZR6ZIvlsAUxfmLXJZERVW8vEvvOCoRBvyYOKV',
-    access_token_secret:  'NqsiGgtA9nt35hyPna40Go28EuhaQtCnXA7UKq3ukS6Rm',
-    //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-   // strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  });
+    //ler file:
+    const readline = require('readline');
+    const fs = require('fs');
 
-    console.log("bot online");
-   
-    var cont;
+    var myInterface = readline.createInterface({
+        input: fs.createReadStream('constants.txt')
+    });
 
-    while(1){
-        console.log("corpo do twitt:\n")
-        const body = prompt('');
-        //console.log(`Hey there ${name}`);
-        //console.log("corpo do twitt:\n");
-        //var body = readline();
 
-        if(body.toString() === 'exit'){
-            console.log("\na sair");
-            return;
-        
+    var T;
+    var linecount = 0;
+    var ck;
+    var cs;
+    var at;
+    var ats;
+    myInterface.on('line', function (line) {
+
+        linecount++;
+        if(linecount == 1){
+            ck = line;
+        }
+        else if(linecount == 2){
+            cs = line;
+        }
+        if(linecount == 3){
+            at = line;
+        }   
+        if(linecount == 4){
+            ats = line;
         }
 
-       T.post('statuses/update', { status: body }, function(err, data, response) {
-        //console.log(data)
-            if(err){
-                console.log("\nErro a enviar tweet");
-            }
-            else{
-                console.log("\nTweet enviado com sucesso");
-                console.log(data)
-                cont = data.media_id_string;
-            }
+        if(linecount == 4)
+        {
+            console.log(typeof(ck));
             
+            var T = new Twit({
+                consumer_key:         ck,
+                consumer_secret:      cs,
+                access_token:         at,
+                access_token_secret:  ats,
 
-        });
-    }
+            });
+
+            console.log("bot online");
+        
+            var cont;
+
+            while(1){
+                var body;
+
+                console.log("corpo do twitt:\n")
+                    body = prompt('');                   
+                
+                if(body.toString() === 'exit' || body.toString() === 'sair'|| body.toString() === 'quit'){
+                    console.log("\na sair");
+                    return;
+                }
+
+                T.post('statuses/update', { status: body }, function(err, data, response) {
+                //console.log(data)
+                    if(err){
+                        console.log("\nErro a enviar tweet");
+                    }
+                    else{
+                        console.log("\nTweet enviado com sucesso");
+                       // console.log(data)
+                        cont = data.media_id_string;
+                    }
+
+                });    
+            }
+        }
+    }); 
+    
